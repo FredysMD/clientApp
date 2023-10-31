8b5301d5b1f963240e49023efd92f3cbda2c95ae
Fredys <fmarquezduque@gmail.com>
2023-10-30 22:57:38 -0500

    Test 7

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -5,7 +5,7 @@ require_once PROJECT_ROOT_PATH . "./index.php";
 // include main jwt class 
 require_once PROJECT_ROOT_PATH . "/utils/JWT.php";
 require_once PROJECT_ROOT_PATH . "/utils/helper.php"; 
-// include the base controller file 
+// include the base controller file  
 require_once PROJECT_ROOT_PATH . "/controllers/BaseController.php";
 // include the base models file 
 require_once PROJECT_ROOT_PATH . "/models/UserModel.php";
