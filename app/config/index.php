06d7bc5e570bc9ca1cf0762d3056fb44d4cb88c0
Fredys <fmarquezduque@gmail.com>
2023-10-31 22:23:37 -0500

    TEST 07

--- app/config/index.php
+++ app/config/index.php
@@ -1,6 +1,6 @@
 <?php 
 define("DB_HOST", "localhost");  
-define("DB_USERNAME", "root");
+define("DB_USERNAME", "root"); 
 define("DB_PASSWORD", "");
 define("DB_DATABASE_NAME", "clientapi");
 define("JWT_SECRET", "F-JaNdRgUkXp2r5u8x/A?D(G+KbPeShVmYq3t6w9y$B&E)H@McQfTjWnZr4u7x!A");
