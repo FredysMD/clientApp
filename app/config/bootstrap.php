6463a361f4809439d6d0165cc324d8157f25524a
Fredys Márquez Duque <fmarquezduque@gmail.com>
2024-04-16 11:35:40 -0500

    test

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -1,7 +1,7 @@
 <?php 
 define("PROJECT_ROOT_PATH", __DIR__ . "/../");   
 // include main configuration file 
-require_once PROJECT_ROOT_PATH . "./index.php";      
+require_once PROJECT_ROOT_PATH . "./index.php";         
 // include main jwt class  
 require_once PROJECT_ROOT_PATH . "/utils/JWT.php";
 require_once PROJECT_ROOT_PATH . "/utils/helper.php";  
